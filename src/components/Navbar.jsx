4c884383494ceb196b1ba407824f895a7fecff4b
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-02-17 21:48:09 +0530

    fixed layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Navbar() {
   return (
     <div className="min-h-[3rem] p-2 shadow-lg bg-gray-400 text-3xl pl-4 text-white">
+      <ShoppingCartIcon sx={{fontSize:'2rem',mt:-1}}/>
       Ecommerce
     </div>
   );
